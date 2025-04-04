a67fcb69544148d9e6828521873049e1d95e16dd
95hanho <hosung6674@naver.com>
2025-04-04 14:31:03 +0900

    example-properties

--- src/main/java/me/_hanho/api_spring_default/controller/AuthController.java
+++ src/main/java/me/_hanho/api_spring_default/controller/AuthController.java
@@ -73,8 +73,8 @@ public class AuthController {
 		} else {
 			User onlyId = new User();
 			onlyId.setId(checkUser.getId());
-			String accessToken = tokenService.makeJwtToken(10, onlyId);
-			String refreshToken = tokenService.makeJwtToken(30);
+			String accessToken = tokenService.makeJwtToken(600, onlyId);
+			String refreshToken = tokenService.makeJwtToken(1800);
 			String ipAddress = request.getRemoteAddr();
 			Token token = new Token(ipAddress, agent, refreshToken, checkUser.getId());
 			authService.insertToken(token);
@@ -149,8 +149,8 @@ public class AuthController {
 			if(checkUser != null) {
 				User onlyId = new User();
 				onlyId.setId(checkUser.getId());
-				String accessToken = tokenService.makeJwtToken(10, onlyId);
-				String refreshToken = tokenService.makeJwtToken(30);
+				String accessToken = tokenService.makeJwtToken(600, onlyId);
+				String refreshToken = tokenService.makeJwtToken(1800);
 				Token token2 = new Token(ipAddress, agent, refreshToken, checkUser.getId());
 				authService.updateToken(token2);
 				
